37ef264e263e6c8655b1a43b2c1e7f0d2b1ac13b
E94086254 <105733132+E94086254@users.noreply.github.com>
2022-09-07 15:19:17 +0800

    Add files via upload

---
+++ gps_port.ino
@@ -0,0 +1,385 @@
+/*!
+ * @file QMC5883_compass.cpp
+ * @brief The program shows how to realize the function compass.When the program runs, please spin QMC5883 freely to accomplish calibration.
+ * @n 3-Axis Digital Compass IC
+ *
+ * @copyright    [DFRobot](http://www.dfrobot.com), 2017
+ * @copyright    GNU Lesser General Public License
+ *
+ * @author [dexian.huang](952838602@qq.com)
+ * @version  V1.0
+ * @date  2017-7-3
+ */
+#include <Wire.h>
+#include <DFRobot_QMC5883.h>
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//  MARVELMIND HEDGEHOG RELATED PART
+
+float hedgehog_x, hedgehog_y;// coordinates of hedgehog (X,Y), mm
+float hedgehog_z;// height of hedgehog, mm
+int hedgehog_pos_updated;// flag of new data from hedgehog received
+
+bool high_resolution_mode;
+
+///
+
+//#define DISTANCE_SENSOR_ENABLED
+
+#define HEDGEHOG_BUF_SIZE 40 
+#define HEDGEHOG_CM_DATA_SIZE 0x10
+#define HEDGEHOG_MM_DATA_SIZE 0x16
+byte hedgehog_serial_buf[HEDGEHOG_BUF_SIZE];
+byte hedgehog_serial_buf_ofs;
+
+#define POSITION_DATAGRAM_ID 0x0001
+#define POSITION_DATAGRAM_HIGHRES_ID 0x0011
+unsigned int hedgehog_data_id;
+
+typedef union {byte b[2]; unsigned int w;int wi;} uni_8x2_16;
+typedef union {byte b[4];float f;unsigned long v32;long vi32;} uni_8x4_32;
+
+//    Marvelmind hedgehog support initialize
+void setup_hedgehog() 
+{
+  Serial.begin(115200); // hedgehog transmits data on 9600bps  
+
+  hedgehog_serial_buf_ofs= 0;
+  hedgehog_pos_updated= 0;
+}
+
+// Marvelmind hedgehog service loop
+void loop_hedgehog()
+{int incoming_byte;
+ int total_received_in_loop;
+ int packet_received;
+ bool good_byte;
+ byte packet_size;
+ uni_8x2_16 un16;
+ uni_8x4_32 un32;
+
+  total_received_in_loop= 0;
+  packet_received= 0;
+  
+  while(Serial.available() > 0)
+    {
+      
+      if (hedgehog_serial_buf_ofs>=HEDGEHOG_BUF_SIZE) 
+      {
+        hedgehog_serial_buf_ofs= 0;// restart bufer fill
+        break;// buffer overflow
+      }
+      total_received_in_loop++;
+      if (total_received_in_loop>100) break;// too much data without required header
+      
+      incoming_byte= Serial.read();
+     
+      good_byte= false;
+      switch(hedgehog_serial_buf_ofs)
+      {
+        case 0:
+        {
+          good_byte= (incoming_byte = 0xff);
+          break;
+        }
+        case 1:
+        {
+          good_byte= (incoming_byte = 0x47);
+          break;
+        }
+        case 2:
+        {
+          good_byte= true;
+          break;
+        }
+        case 3:
+        {
+          hedgehog_data_id= (((unsigned int) incoming_byte)<<8) + hedgehog_serial_buf[2];
+          good_byte=   (hedgehog_data_id == POSITION_DATAGRAM_ID) ||
+                       (hedgehog_data_id == POSITION_DATAGRAM_HIGHRES_ID);
+          break;
+        }
+        case 4:
+        {
+          switch(hedgehog_data_id)
+          {
+            case POSITION_DATAGRAM_ID:
+            {
+              good_byte= (incoming_byte == HEDGEHOG_CM_DATA_SIZE);
+              break;
+            }
+            case POSITION_DATAGRAM_HIGHRES_ID:
+            {
+              good_byte= (incoming_byte == HEDGEHOG_MM_DATA_SIZE);
+              break;
+            }
+          }
+          break;
+        }
+        default:
+        {
+          good_byte= true;
+          break;
+        }
+      }
+      
+      if (!good_byte)
+        {
+          hedgehog_serial_buf_ofs= 0;// restart bufer fill         
+          continue;
+        }     
+      hedgehog_serial_buf[hedgehog_serial_buf_ofs++]= incoming_byte; 
+      if (hedgehog_serial_buf_ofs>5)
+        {
+          packet_size=  7 + hedgehog_serial_buf[4];
+          if (hedgehog_serial_buf_ofs == packet_size)
+            {// received packet with required header
+              packet_received= 1;
+              hedgehog_serial_buf_ofs= 0;// restart bufer fill
+              break; 
+            }
+        }
+    }
+
+  if (packet_received)  
+    {
+      hedgehog_set_crc16(&hedgehog_serial_buf[0], packet_size);// calculate CRC checksum of packet
+      if ((hedgehog_serial_buf[packet_size] == 0)&&(hedgehog_serial_buf[packet_size+1] == 0))
+        {// checksum success
+          switch(hedgehog_data_id)
+          {
+            case POSITION_DATAGRAM_ID:
+            {
+              // coordinates of hedgehog (X,Y), cm ==> mm
+              un16.b[0]= hedgehog_serial_buf[9];
+              un16.b[1]= hedgehog_serial_buf[10];
+              hedgehog_x= 10*long(un16.wi);
+              un16.b[0]= hedgehog_serial_buf[11];
+              un16.b[1]= hedgehog_serial_buf[12];
+              hedgehog_y= 10*long(un16.wi);
+              // height of hedgehog, cm==>mm (FW V3.97+)
+              un16.b[0]= hedgehog_serial_buf[13];
+              un16.b[1]= hedgehog_serial_buf[14];
+              hedgehog_z= 10*long(un16.wi);
+              
+              hedgehog_pos_updated= 1;// flag of new data from hedgehog received
+              high_resolution_mode= false;
+              break;
+            }
+
+            case POSITION_DATAGRAM_HIGHRES_ID:
+            {
+              // coordinates of hedgehog (X,Y), mm
+              un32.b[0]= hedgehog_serial_buf[9];
+              un32.b[1]= hedgehog_serial_buf[10];
+              un32.b[2]= hedgehog_serial_buf[11];
+              un32.b[3]= hedgehog_serial_buf[12];
+              hedgehog_x= un32.vi32;
+
+              un32.b[0]= hedgehog_serial_buf[13];
+              un32.b[1]= hedgehog_serial_buf[14];
+              un32.b[2]= hedgehog_serial_buf[15];
+              un32.b[3]= hedgehog_serial_buf[16];
+              hedgehog_y= un32.vi32;
+              
+              // height of hedgehog, mm 
+              un32.b[0]= hedgehog_serial_buf[17];
+              un32.b[1]= hedgehog_serial_buf[18];
+              un32.b[2]= hedgehog_serial_buf[19];
+              un32.b[3]= hedgehog_serial_buf[20];
+              hedgehog_z= un32.vi32;
+              
+              hedgehog_pos_updated= 1;// flag of new data from hedgehog received
+              high_resolution_mode= true;
+              break;
+            }
+          }
+        } 
+    }
+}
+
+// Calculate CRC-16 of hedgehog packet
+void hedgehog_set_crc16(byte *buf, byte size)
+{uni_8x2_16 sum;
+ byte shift_cnt;
+ byte byte_cnt;
+
+  sum.w=0xffffU;
+
+  for(byte_cnt=size; byte_cnt>0; byte_cnt--)
+   {
+   sum.w=(unsigned int) ((sum.w/256U)*256U + ((sum.w%256U)^(buf[size-byte_cnt])));
+
+     for(shift_cnt=0; shift_cnt<8; shift_cnt++)
+       {
+         if((sum.w&0x1)==1) sum.w=(unsigned int)((sum.w>>1)^0xa001U);
+                       else sum.w>>=1;
+       }
+   }
+
+  buf[size]=sum.b[0];
+  buf[size+1]=sum.b[1];// little endian
+}// hedgehog_set_crc16
+
+//  END OF MARVELMIND HEDGEHOG RELATED PART
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+#define CM 1      //Centimeter
+#define INC 0     //Inch
+#define TP 2      //Trig_pin
+#define EP 3      //Echo_pin
+int i=0;
+int a_flag=0;//a point flag
+float a_x=4.3;
+float a_y=0;
+float headingDegrees=0;
+float Degrees=0;
+
+DFRobot_QMC5883 compass;
+void setup()
+{
+  setup_hedgehog();//    Marvelmind hedgehog support initialize
+  Serial.begin(115200);
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+  while (!compass.begin())
+  {
+    Serial.println("Could not find a valid QMC5883 sensor, check wiring!");
+    delay(500);
+  }
+  if(compass.isHMC()){
+        Serial.println("Initialize HMC5883");
+        compass.setRange(HMC5883L_RANGE_1_3GA);
+        compass.setMeasurementMode(HMC5883L_CONTINOUS);
+        compass.setDataRate(HMC5883L_DATARATE_15HZ);
+        compass.setSamples(HMC5883L_SAMPLES_8);
+    }
+   else if(compass.isQMC()){
+        Serial.println("Initialize QMC5883");
+        compass.setRange(QMC5883_RANGE_2GA);
+        compass.setMeasurementMode(QMC5883_CONTINOUS); 
+        compass.setDataRate(QMC5883_DATARATE_50HZ);
+        compass.setSamples(QMC5883_SAMPLES_8);
+   }
+  }
+void loop()
+{
+  loop_hedgehog();// Marvelmind hedgehog service loop
+  Vector norm = compass.readNormalize();
+  // Calculate heading
+  float heading = atan2(norm.YAxis, norm.XAxis);
+  // Set declination angle on your location and fix heading
+  // You can find your declination on: http://magnetic-declination.com/
+  // (+) Positive or (-) for negative
+  // For Bytom / Poland declination angle is 4'26E (positive)
+  // Formula: (deg + (min / 60.0)) / (180 / M_PI);
+  float declinationAngle = ((-4.0) + (17.0 / 60.0)) / (180 / PI);
+  heading += declinationAngle;
+  // Correct for heading < 0deg and heading > 360deg
+  if (heading < 0){
+    heading += 2 * PI;
+  }
+  if (heading > 2 * PI){
+    heading -= 2 * PI;
+  }
+  // Convert to degrees
+  headingDegrees = heading * 180/PI; 
+  // Output
+  if(heading>0)
+  {
+    i++;
+  }
+  if(i>50)
+  {
+    Serial.print("hedgehog_x:");
+    Serial.print(hedgehog_x/1000);
+    Serial.print("hedgehog_y:");
+    Serial.println(hedgehog_y/1000);
+    Serial.print(" Heading = ");
+    Serial.print(heading);
+    Serial.print(" Degress = ");
+    Serial.print(headingDegrees);
+    Degrees=headingDegrees;
+    Serial.println();
+    if(a_flag==0)// not arrive to a_y
+      go_to_ay();
+    //if(a_flag==1)// not arrive to a_x
+      //go_to_ax();
+    i=0;
+  }
+}
+long Distance(long time, int flag)
+{
+  long distacne;
+  if(flag)
+    distacne = time /29 / 2  ;     // Distance_CM  = ((Duration of high level)*(Sonic :340m/s))/2
+                                   //              = ((Duration of high level)*(Sonic :0.034 cm/us))/2
+                                   //              = ((Duration of high level)/(Sonic :29.4 cm/us))/2
+  else
+    distacne = time / 74 / 2;      // INC
+  return distacne;
+}
+
+long TP_init()
+{                     
+  digitalWrite(TP, LOW);                    
+  delayMicroseconds(2);
+  digitalWrite(TP, HIGH);                 // pull the Trig pin to high level for more than 10us impulse 
+  delayMicroseconds(10);
+  digitalWrite(TP, LOW);
+  long microseconds = pulseIn(EP,HIGH);   // waits for the pin to go HIGH, and returns the length of the pulse in microseconds
+  return microseconds;                    // return microseconds
+}
+
+void requestEvent() {
+  int c1 = 23;
+  //hedgehog_x = 5;
+  int c =  hedgehog_x*1000;
+  Wire.write(c);
+  Wire.write("s");
+  // as expected by master
+}
+void go_to_ay()
+{
+  if((0<Degrees<40)||(320<headingDegrees<360))//車頭朝窗
+    {
+      if(a_y-(hedgehog_y/1000)>0.4)//現在座標與a點差40CM
+      {
+        Serial.print("g");
+      }
+      else
+      {
+        a_flag=1;//arrive to a_y   
+        Serial.print(a_flag);
+      }
+    }
+  else if(180>Degrees>40)
+    {
+      Serial.print("l");
+    }
+  else if(180<Degrees<320)
+    {
+      Serial.print("r");
+    }
+}
+void go_to_ax()
+{
+  if(80<headingDegrees<120)//車頭朝電梯
+  {
+    if(a_x-(hedgehog_x/1000)>0.4)//現在座標與a點差40CM
+      Serial.print("g");
+    else
+      a_flag=2;//arrive to a_y  
+  }
+  else if(headingDegrees<80)
+  {
+    Serial.print("r");
+  }
+  else if(headingDegrees>120)
+  {
+    Serial.print("l");
+  }
+}
